fe0c02b40784f5061b884d5dfd0526cb89762a94
Juan Felipe <jufeplasa@hotmail.com>
2021-09-06 14:40:16 -0500

    third commit

--- La_Cucharita/src/ui/Main.java
+++ La_Cucharita/src/ui/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("Authentication.fxml"));
+		FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("Login.fxml"));
 		Parent root = fxmlLoader.load();
 		Scene scene= new Scene(root);
 		primaryStage.setScene(scene);;
